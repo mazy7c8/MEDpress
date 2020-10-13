d7b1415dec3cf838719e2e21fef28a04d6194229
mazy7c8 <rafalinz@gmail.com>
2020-10-13 22:33:14 +0200

    visual blink for key events

--- main.py
+++ main.py
@@ -11,9 +11,6 @@ class MEDpress(object):
         self.root = parent
         self.root.title("Ekran Glowny")
         self.frame = tk.Frame(parent)
-        self.root.bind("<Control-s>",lambda event, : self.getTextEntry())
-        self.root.bind("<Control-z>",lambda event, : self.readWork())
-        self.root.bind("<Control-c>",lambda event, : self.copyToClipboard())
         self.frame.pack()
         self.root['bg']="#FCAFAF"
 
@@ -155,27 +152,30 @@ class MEDpress(object):
         savebutton.pack()
         savebutton.place(y=224,x=320)
 
-        startbutton = tk.Button(
+        self.startbutton = tk.Button(
             self.frame,
             text="Rozpocznij wypis",
             width=14,
             height=2,
             bg="lightgrey",
+            activebackground='#00ff00',
             command=self.getTextEntry
         )
-        startbutton.pack()
-        startbutton.place(x=840,y=201)
+        self.startbutton.pack()
+        self.startbutton.place(x=840,y=201)
 
-        clipboardbutton = tk.Button(
+        self.clipboardbutton = tk.Button(
             self.frame,
             text="Do schowka",
             width=14,
             height=2,
             bg="lightgrey",
+            activebackground='#00ff00',
             command = self.copyToClipboard
         )
-        clipboardbutton.pack()
-        clipboardbutton.place(x=998,y=201)
+        self.clipboardbutton.pack()
+        self.clipboardbutton.place(x=998,y=201)
+
 
         exportbutton = tk.Button(
             self.frame,
@@ -200,18 +200,25 @@ class MEDpress(object):
         progresslabel.pack()
         progresslabel.place(x=1020,y=850)
 
-        endworkbutton = tk.Button(
+        self.endworkbutton = tk.Button(
             self.frame,
             text="Zakończ",
             width=10,
             height=2,
             bg="lightgrey",
+            activebackground='#00ff00',
             command=self.readWork
         )
-        endworkbutton.pack()
-        endworkbutton.place(x=1175,y=839)
+        self.endworkbutton.pack()
+        self.endworkbutton.place(x=1175,y=839)
+
+        self.root.bind("<Control-s>",lambda event, : self.getTextEntry())
+        self.root.bind("<Control-z>",lambda event, : self.readWork())
+        self.root.bind("<Control-c>",lambda event, : self.copyToClipboard())
 
     def copyToClipboard(self):
+        self.clipboardbutton.config(bg="#00FF00")
+        root.after(100, lambda: self.clipboardbutton.config(bg = 'lightgrey'))
         textEntry=self.textfield.get("1.0", "end-1c")
         try:
             #MACOS
@@ -231,6 +238,8 @@ class MEDpress(object):
             return subprocess.check_call(cmd, shell=True)
 
     def getTextEntry(self):
+        self.startbutton.config(bg="#00FF00")
+        root.after(100, lambda: self.startbutton.config(bg = 'lightgrey'))
         textEntry=self.textfield.get("1.0", "end-1c")
         #print(textEntry)
         self.templateSearch(textEntry)
@@ -335,10 +344,13 @@ class MEDpress(object):
         return varentry
 
     def readWork(self):
+        self.endworkbutton.config(bg="#00FF00")
+        root.after(100, lambda: self.endworkbutton.config(bg = 'lightgrey'))
         readed={}
         for keys in self.entryBoxList:
             readed[keys]=self.entryBoxList[keys].get()
         self.initializeRender(self.found,readed)
+    
             
 
 
